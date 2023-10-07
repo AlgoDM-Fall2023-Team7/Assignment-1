26a71cb4cd114784cba2c16565ecaff3649793b9
Aishwarya SVS <130128458+aishwaryasvs@users.noreply.github.com>
2023-10-07 01:19:23 -0400

    setting title of the streamlit app

--- Task-3/main.py
+++ Task-3/main.py
@@ -7,3 +7,7 @@ import pandas as pd
 from queries import queries  # Import the queries dictionary
 
 load_dotenv()  # Load environment variables from .env
+
+
+# Set the title of the app
+st.sidebar.markdown("<h1 style='text-align: center;'>FlakeQuery</h1>", unsafe_allow_html=True)
